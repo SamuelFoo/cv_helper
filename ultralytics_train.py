abe6e3e5ae91bf31c38595cac23606d82ecc0b9a
SamuelFoo <fooenzesamuel@gmail.com>
2024-05-31 06:57:18 +0800

    Disable flipping, make deterministic and profile.

--- ultralytics_train.py
+++ ultralytics_train.py
@@ -10,5 +10,19 @@ model = YOLO(
 #     "models/YOLO/yolov8n.pt"
 # )  # build from YAML and transfer weights
 
+hyperparameters = {
+    "fliplr": 0.0,  # Disable flipping left-right as it does not make sense for numbers
+}
+
 # Train the model
-model.train(data="front_cam.yaml", epochs=200, imgsz=640, patience=0, batch=-1)
+model.train(
+    data="front_cam.yaml",
+    epochs=200,
+    imgsz=640,
+    patience=0,
+    batch=-1,
+    deterministic=False,
+    profile=True,
+    augment=True,
+    **hyperparameters,
+)
