b22ec717bd3067310ba35e93a3da5a008d9e03ba
SamuelFoo <fooenzesamuel@gmail.com>
2024-06-06 01:07:44 +0800

    Add shear and perspective to hyperparameters tuning

--- ultralytics_tune.py
+++ ultralytics_tune.py
@@ -3,12 +3,17 @@ from pathlib import Path
 from ultralytics import YOLO
 
 # Initialize the YOLO model
-model = YOLO("weights/yolov8n_310524_1.pt")
+# model = YOLO("weights/yolov8n_310524_1.pt")
+model = YOLO("yolov8n.yaml").load(
+    "models/YOLO/yolov8n.pt"
+)  # build from YAML and transfer weights
 
 # Set starting tuning hyperparameters
-# hyperparameters = {
-#     "fliplr": 0.0,  # Disable flipping left-right as it does not make sense for numbers
-# }
+hyperparameters = {
+    "fliplr": 0.0,  # Disable flipping left-right as it does not make sense for numbers
+    "shear": 10,
+    "perspective": 0.0005,
+}
 
 # Load hyperparameters
 hyperparameters_file_path = (
@@ -27,5 +32,6 @@ model.tune(
     save=False,
     val=False,
     batch=-1,
-    cfg=hyperparameters_file_path,
+    **hyperparameters
+    # cfg=hyperparameters_file_path,
 )
